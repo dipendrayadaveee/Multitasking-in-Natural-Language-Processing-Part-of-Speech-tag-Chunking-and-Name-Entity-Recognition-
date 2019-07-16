71c9b8a47cdfe589d110166c4353a57df8be3568
Dipendra Yadav <dipendra.yadav@planet-ai.de>
2019-07-16 10:47:34 +0200

    included the evaluation for NER also, Results for NER is good but doesnt seem to work for Chunk and Pos

--- generate_results.py
+++ generate_results.py
@@ -12,11 +12,18 @@ def generate_results(path):
     chunk_val = path + 'chunk_pred_val.txt'
     chunk_comb = path + 'chunk_pred_combined.txt'
     chunk_test = path + 'chunk_pred_test.txt'
+
+    ner_train = path + 'ner_pred_train.txt'
+    ner_val = path + 'ner_pred_val.txt'
+    ner_comb = path + 'ner_pred_combined.txt'
+    ner_test = path + 'ner_pred_test.txt'
+
     pos_train = path + 'pos_pred_train.txt'
     pos_val = path + 'pos_pred_val.txt'
     pos_comb = path + 'pos_pred_combined.txt'
     pos_test = path + 'pos_pred_test.txt'
 
+#for chunk results
     print('generating latex tables - chunk train')
     cmd = 'perl eval.pl -l < ' + chunk_train
     os.system(cmd)
@@ -33,6 +40,24 @@ def generate_results(path):
     cmd = 'perl eval.pl -l < ' + chunk_test
     os.system(cmd)
 
+#For NER results
+    print('generating latex tables - ner train')
+    cmd = 'perl eval.pl -l < ' + ner_train
+    os.system(cmd)
+
+    print('generating latex tables - ner valid')
+    cmd = 'perl eval.pl -l < ' + ner_val
+    os.system(cmd)
+
+    print('generating latex tables - ner combined')
+    cmd = 'perl eval.pl -l < ' + ner_comb
+    os.system(cmd)
+
+    print('generating latex tables - ner test')
+    cmd = 'perl eval.pl -l < ' + ner_test
+    os.system(cmd)
+
+#For PoS results
     print('generating accuracy - pos train')
     print(pos_eval(pos_train))
 
